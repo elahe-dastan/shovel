c305e03a9bb411167ca5bdfd60f4c87a801e3120
elahe dastan <elahe.dstn@gmail.com>
2020-11-21 18:20:24 +0330

    remove null values

--- HW2/titanic.py
+++ HW2/titanic.py
@@ -1,4 +1,18 @@
 import pandas as pd
 
+# Read data
 train_data = pd.read_csv("data/train.csv")
-print(train_data.info())
+# print(train_data.info())
+
+# Fix null values
+
+# null age --> median
+age = train_data['Age']
+age.fillna(age.median, inplace=True)
+
+# null embarked --> mode
+embarked = train_data['Embarked']
+embarked.fillna(embarked.mode()[0], inplace=True)
+
+# drop cabin
+train_data = train_data.drop('Cabin', axis=1)

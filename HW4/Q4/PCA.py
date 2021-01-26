1fec7a91f701db0c5bc2c799c7f8e03edf327175
elahe dastan <elahe.dstn@gmail.com>
2021-01-27 01:44:14 +0330

    pca

--- HW4/Q4/PCA.py
+++ HW4/Q4/PCA.py
@@ -1,7 +1,22 @@
 from sklearn.decomposition import PCA
+import numpy as np
 
 f = open("glove.6B/glove.6B.50d.txt", "r")
 lines = {}
+wordss = []
+a = np.zeros((1000, 50))
 for i in range(1000):
     line = f.readline()
-    
+    if line.endswith('\n'):
+        line = line[:-1]
+    words = line.split(" ")
+    a[i] = words[1:]
+    wordss.append(words[0])
+    lines[words[0]] = words[1:]
+# print(lines)
+pca = PCA(n_components=2)
+b = pca.fit_transform(a)
+for i in range(1000):
+    print(wordss[i])
+    print(b[i])
+# print(b)
